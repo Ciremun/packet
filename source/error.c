8de3a5ae36ddffa2b2ba67b352c2e3d157ecc64e
Ciremun <Ciremun@yandex.ru>
2022-08-22 15:00:41 +0300

    \n for errors

--- source/error.c
+++ source/error.c
@@ -5,17 +5,17 @@ void print_sock_error(const char *error_name, const char *file, int line)
 #ifndef NDEBUG
 
 #ifdef _WIN32
-    fprintf(stderr, "%s:%d - %s: %ld", file, line, error_name, WSAGetLastError());
+    fprintf(stderr, "%s:%d - %s: %ld\n", file, line, error_name, WSAGetLastError());
 #else
-    fprintf(stderr, "%s:%d - %s: %s", file, line, error_name, strerror(errno));
+    fprintf(stderr, "%s:%d - %s: %s\n", file, line, error_name, strerror(errno));
 #endif // _WIN32
 
 #else
 
 #ifdef _WIN32
-    fprintf(stderr, "%s: %ld", error_name, WSAGetLastError());
+    fprintf(stderr, "%s: %ld\n", error_name, WSAGetLastError());
 #else
-    fprintf(stderr, "%s: %s", error_name, strerror(errno));
+    fprintf(stderr, "%s: %s\n", error_name, strerror(errno));
 #endif // _WIN32
 
 #endif // NDEBUG
